20ce19363e6b99f8933b09ebbe7071ce49e52664
Márcio Mendes <mmp.mendes@campus.fct.unl.pt>
2018-07-04 14:33:41 +0000

    First commit after a while

---
+++ GEstAP_API/Models/EntidadeNode.cs
@@ -0,0 +1,45 @@
+﻿using EstruturaAP_DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace GEstAP_API.Models
+{
+    public class EntidadeNode
+    {
+        [DataMember]
+        public int CodEstruturaInterno { get; set; }
+        [DataMember]
+        public string CodEstrutura { get; set; }
+        [DataMember]
+        public int CodEntidade { get; set; }
+        [DataMember]
+        public string Designacao { get; set; }
+        [DataMember]
+        public string Sigla { get; set; }
+        [DataMember]
+        public int? Supervisor { get; set; }
+        [DataMember]
+        public List<EntidadeNode> children { get; set; }
+
+        public EntidadeNode()
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="est"></param>
+        public EntidadeNode(Estrutura est)
+        {
+            CodEntidade = est.CodEntidade;
+            CodEstrutura = est.CodEstrutura;
+            CodEstruturaInterno = est.CodEstruturaInterno;
+            Designacao = est.Entidade?.Designacao;
+            Sigla = est.Entidade?.Sigla;
+            Supervisor = est.Supervisor?.Supervisor?.CodEstruturaInterno;
+        }
+    }
+}
